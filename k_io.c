bc321f00fe2278b34cbde9aec8fc19d3960fe238
DavidTX2000 <davidtx2000ofc@gmail.com>
2026-04-21 07:25:30 -0300

    Update k_io.c

--- k_io.c
+++ k_io.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Função que o seu Kernel vai chamar para capturar texto
 void k_input(const char* prompt, char* output_var) {
     printf("%s", prompt);
     scanf("%s", output_var);
